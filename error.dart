a7ea770ceccf5131e1b3e8f2b22abf66f41859e2
benrich <benrich.prince@cinfores.com>
2026-09-10 11:40:36 +0100

    funds

--- error.dart
+++ error.dart
@@ -1,8 +1,8 @@
-class Error {
-  [
-    String? message,
-    int? code,
-  ]) {
-    this.message = message;
-    this.code = code;
-  }nsfjsjfsdfs
+// class Error {
+//   [
+//     String? message,
+//     int? code,
+//   ]) {
+//     this.message = message;
+//     this.code = code;
+//   }nsfjsjfsdfs
