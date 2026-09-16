61e37209373f67af6e10fac0913a3f327dd49bd2
kencodeit <kenchinnah@gmail.com>
2026-09-16 13:45:27 +0100

    pswdTest

---
+++ ConditionalStmt.dart
@@ -0,0 +1,11 @@
+// CONDITIONAL STATEMENTS
+void main() {
+  var account_balance = 100;
+  var receiver = "Dodon";
+
+  if (account_balance > 50) {
+    print("Transfer of $account_balance to $receiver successful");
+  } else {
+    print("transfer failed");
+  }
+}
