24f05cce217e3af1a5c5fea2a96f7d53743c1728
benrich <benrich.prince@cinfores.com>
2026-09-16 15:04:32 +0100

    push ui edit

---
+++ function.dart
@@ -0,0 +1,124 @@
+transfer() {
+  {
+    print("transfer of 500 is successful  ");
+  }
+}
+
+getpaidhouserent() {
+  int james = 100;
+  int alex = 200;
+
+  print("${alex + james} has been paid in house rent");
+}
+
+getHasNotPaidHouseRent() {
+  print("lizzy has not paid his house rent");
+  print("peter has not paid his house rent");
+}
+
+names() {
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+  print("benrich");
+  print("09063420516");
+  print("amos");
+  print("mark");
+  print("james");
+  print("angela");
+  print("paul");
+}
+//fff
+void main() {
+  
+   getHasNotPaidHouseRent();
+   getpaidhouserent();
+}
