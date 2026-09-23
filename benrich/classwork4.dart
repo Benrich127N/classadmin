1f3539f222416fb59277e877aa4f2472744f6d85
clinton <clint4achange@gmail.com>
2026-09-23 12:41:32 +0100

    classwo

---
+++ benrich/classwork4.dart
@@ -0,0 +1,10 @@
+void hello(String name)
+{
+  print("hello, $name");
+  
+ 
+}
+
+  void main()
+  {
+  hello("Fredmond");}
--- functionnumber.dart
+++ functionnumber.dart
@@ -2,5 +2,3 @@ int addnumbers(int x, int y) => x + y;
 void main() {
   print(addnumbers( 30, 50));
 }
-
-int subtractnumber( int x, int y, int z) => x - y - z;
